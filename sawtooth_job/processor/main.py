8ea5fd69c0374d6fddd69f4a6469a6d8014cd027
collaedge <a125614418@outlook.com>
2020-06-23 15:44:59 +1000

    Update main.py

--- sawtooth_job/processor/main.py
+++ sawtooth_job/processor/main.py
@@ -18,11 +18,61 @@ from sawtooth_job.processor.handler import JobTransactionHandler
 
 DISTRIBUTION_NAME = 'sawtooth-job'
 
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter)
+
+    parser.add_argument(
+        '-C', '--connect',
+        default='tcp://localhost:4004',
+        help='Endpoint for the validator connection')
+
+    parser.add_argument('-v', '--verbose',
+                        action='count',
+                        default=0,
+                        help='Increase output sent to stderr')
+
+    return parser.parse_args(args)
+
+
 def main(args=None):
-    processor = TransactionProcessor(url='tcp://127.0.0.1:4004')
+    if args is None:
+        args = sys.argv[1:]
+    opts = parse_args(args)
+    processor = None
+    try:
+        processor = TransactionProcessor(url=opts.connect)
+        log_config = get_log_config(filename="intkey_log_config.toml")
 
-    handler = JobTransactionHandler()
+        # If no toml, try loading yaml
+        if log_config is None:
+            log_config = get_log_config(filename="intkey_log_config.yaml")
 
-    processor.add_handler(handler)
+        if log_config is not None:
+            log_configuration(log_config=log_config)
+        else:
+            log_dir = get_log_dir()
+            # use the transaction processor zmq identity for filename
+            log_configuration(
+                log_dir=log_dir,
+                name="job-" + str(processor.zmq_id)[2:-1])
 
-    processor.start()
+        init_console_logging(verbose_level=opts.verbose)
+
+        # The prefix should eventually be looked up from the
+        # validator's namespace registry.
+        handler = JobTransactionHandler()
+
+        processor.add_handler(handler)
+
+        processor.start()
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:  # pylint: disable=broad-except
+        print("Error: {}".format(e), file=sys.stderr)
+    finally:
+        if processor is not None:
+            processor.stop()
+
+   
