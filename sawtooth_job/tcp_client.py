4fa171a98fa7d486899d8bbf789668653cf8ca2d
collaedge <a125614418@outlook.com>
2020-07-22 15:17:59 +1000

    update

--- sawtooth_job/tcp_client.py
+++ sawtooth_job/tcp_client.py
@@ -1,30 +1,50 @@
+from tcp_server import send,receive
 from socket import *
-from time import ctime
-import select
 import sys
+import select
+import cPickle
+import struct
+import signal
  
-HOST = 'localhost'
-PORT = 21567
-BUFSIZ = 1024
-ADDR = (HOST, PORT)
-tcpCliSock = socket(AF_INET, SOCK_STREAM)
-tcpCliSock.connect(ADDR)
-input = [tcpCliSock,sys.stdin]
- 
-while True:
-	readyInput,readyOutput,readyException = select.select(input,[],[])
-	for indata in readyInput:
-		if indata==tcpCliSock:
-			data = tcpCliSock.recv(BUFSIZ)
-			print(data)
-			if data=='88':
-				break	
-		else:
-			data = raw_input('>')
-			if data=='88':	
-				tcpCliSock.send('%s' %(data))
-				break
-			tcpCliSock.send('[%s] %s' %(ctime(), data))
-	if data=='88':	
-		break
-tcpCliSock.close()
+class ChatClient(object):
+  def __init__(self,name):
+    self.name = name
+    self.connected = False
+    self.host = 'localhost'
+    self.port = 6004
+    try:
+      self.sock = socket(AF_INET,SOCK_STREAM)
+      self.sock.connect((self.host,self.port))
+      self.connected = True
+      send(self.sock,self.name)
+      data= receive(self.sock)
+      addr = data
+    except error as e:#socket.serro
+      print('Failed to connect to chat server')
+      sys.exit(1)
+  def run(self):
+    while True:
+      try:
+        readable,writeable,exception = select.select([0,self.sock],[],[])
+        for sock in readable:
+          if sock == 0:
+            data = sys.stdin.readline().strip()
+            if data:
+              send(self.sock,data)
+          else:
+            data=receive(self.sock)
+            if not data:
+              print('Client shutting down.')
+              self.connected = False
+              break
+            else:
+              sys.stdout.write(data+'\n')
+              sys.stdout.flush()
+      except KeyboardInterrupt:
+        print('Client interrupted')
+        self.sock.close()
+        break
+if __name__ == "__main__":
+  name = raw_input("Please input login name > ")
+  client=ChatClient(name)
+  client.run()
--- sawtooth_job/tcp_server.py
+++ sawtooth_job/tcp_server.py
@@ -1,41 +1,100 @@
-
-from socket import *
-from time import ctime
-import select
 import sys
- 
+import struct
+import select
+import signal
+import cPickle
+from socket import *
 HOST = ''
-PORT = 21567
-BUFSIZ = 1024
-ADDR = (HOST, PORT)
+def send(channel,*args):
+  buffer = cPickle.dumps(args)
+  value = htonl(len(buffer))
+  size = struct.pack("L",value)
+  channel.send(size)
+  channel.send(buffer)
+def receive(channel):
+  size = struct.calcsize("L")
+  size = channel.recv(size)
+  try:
+    size = ntohl(struct.unpack("L",size)[0])
+  except struct.error as e:
+    return ''
+  buf = ''
+  while len(buf) < size:
+    buf += channel.recv(size-len(buf))
+  return cPickle.loads(buf)[0]
  
-tcpSerSock = socket(AF_INET, SOCK_STREAM)
-tcpSerSock.bind(ADDR)
-tcpSerSock.listen(5)
-input = [tcpSerSock, sys.stdin] 
+class ChatServer(object):
+  def __init__(self,PORT,backlog = 5):
+    self.clients = 0
+    self.clientmap = {}
+    self.outputs = [] 
+    self.server = socket(AF_INET, SOCK_STREAM)
+    self.server.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
+    self.server.bind((HOST,PORT))
+    self.server.listen(backlog)
+    signal.signal(signal.SIGINT,self.signalhandler)
  
-while True:
-	print('waiting for connection...')
-	tcpCliSock, addr = tcpSerSock.accept()
-	print('...connected from:',addr)
-	input.append(tcpCliSock) 
-	while True:
-		readyInput,readyOutput,readyException = select.select(input,[],[])
-		for indata in readyInput:
-			if indata==tcpCliSock:
-				data = tcpCliSock.recv(BUFSIZ)
-				print(data)
-				if data=='88':
-					input.remove(tcpCliSock)
-					break
-			else:
-				data = raw_input('>')
-				if data=='88':
-					tcpCliSock.send('%s' %(data))
-					input.remove(tcpCliSock)
-					break
-				tcpCliSock.send('[%s] %s' %(ctime(), data))
-		if data=='88':
-			break
-	tcpCliSock.close()
-tcpSerSock.close()
+  def signalhandler(self,signum,frame):
+    print("Shutting down server ...")
+    for output in self.outputs:
+      output.close()
+    self.server.close()
+ 
+  def get_client_name(self,client):
+    info = self.clientmap[client]
+    host,port,name = info[0][0],info[0][1],info[1]
+    return ':'.join((('@'.join((name,host))),str(port)))
+ 
+  def run(self):
+    inputs = [self.server]
+    print('Waiting for connect...')
+    while True:
+      try:
+        readable,writeable,execption = select.select(inputs,self.outputs,[])
+      except select.error as e:
+        break
+      for sock in readable:
+        if sock == self.server:
+          client,address = self.server.accept()
+          print("Chat server: connected from",address)
+          self.clients += 1
+          cname = receive(client)
+          send(client,str(address[0]))
+          inputs.append(client)
+          self.clientmap[client] = (address,cname)
+          msg = "(Connected : New Client(%d) from %s)\n"%(self.clients,self.get_client_name(client))
+          message = "At present, only one of you is in the chat room!"
+          if self.clients == 1:
+            send(client,message)
+          for output in self.outputs:
+            send(output,msg)
+          self.outputs.append(client)
+ 
+        #elif sock == sys.stdin:
+          #break
+        else:
+          try:
+            data = receive(sock)
+            if data:
+              msg = '[' + self.get_client_name(sock)+ '] >> ' + data
+              for output in self.outputs:
+                if output!=sock:
+                  send(output,msg)
+            else:
+              self.clients-=1
+              sock.close()
+              inputs.remove(sock)
+              self.outputs.remove(sock)
+              msg = '(Now hung up: Client from %s)'%self.get_client_name(sock)
+              message = "At present, only one of you is in the chat room!"
+              for output in self.outputs:
+                send(output,msg)
+              if self.clients == 1:
+                send(self.outputs[0],message)
+          except error as e:
+            inputs.remove(sock)
+            self.outputs.remove(sock)
+    self.server.close()
+if __name__ == "__main__":
+    server = ChatServer(6004)
+    server.run()
