7b4d93a5a33e4600947e9e74bd4f88556697bc2a
collaedge <a125614418@outlook.com>
2020-08-02 11:26:50 +1000

    Update cpu_usage.py

--- cpu_usage.py
+++ cpu_usage.py
@@ -1,19 +1,28 @@
 import psutil
 import logging
 import time
+import sys
 
-def run():
-    # logging.basicConfig(filename="cpu_usage.log", level=logging.DEBUG)
-    logger = logging.getLogger()
-    hdlr = logging.FileHandler('cpu_usage.log')
-    # formatter = logging.Formatter('%(asctime)s  -  %(message)s')
-    # hdlr.setFormatter(formatter)
-    logger.addHandler(hdlr) 
-    logger.setLevel(logging.DEBUG)
-    while 1:
-        cpu_usage = psutil.cpu_percent(interval=1)
-        t = time.time()
-        info = str(t) + '-' + str(cpu_usage)
-        logger.info(info)
+class CPU:
+    def __init__(self, pid):
+        super().__init__()
+        self.pid = pid
 
-run()
+    def run(self):
+        # logging.basicConfig(filename="cpu_usage.log", level=logging.DEBUG)
+        logger = logging.getLogger()
+        hdlr = logging.FileHandler('cpu_usage.log')
+        # formatter = logging.Formatter('%(asctime)s  -  %(message)s')
+        # hdlr.setFormatter(formatter)
+        logger.addHandler(hdlr) 
+        logger.setLevel(logging.DEBUG)
+        while 1:
+            p = psutil.Process(self.pid)
+            cpu_usage = psutil.cpu_percent(interval=2)
+            t = time.time()
+            info = str(t) + '-' + str(cpu_usage)
+            logger.info(info)
+
+if __name__ == "__main__":
+    cpu=CPU(sys.argv[1])
+    cpu.run()
